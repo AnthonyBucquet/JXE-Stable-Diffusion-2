b731c91c15e706f7ca57448879c15f8b680f8955
Anthony Bucquet <anthony.bucquet@atos.net>
2023-08-24 17:38:49 +0200

    debug

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import torch
+print("a")
 import os
+print("b")
 os.environ["TRANSFORMERS_CACHE"] = "/tmp"
-
+print("c")
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
-
+print("d")
 model_id = "stabilityai/stable-diffusion-2-1"
-
+print("e")
 # Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+print("f")
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+print("g")
 pipe = pipe.to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
